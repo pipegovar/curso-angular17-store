ad56f2fe4bbdfcf402beaf9964a7d301a4c20c79
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-09-20 17:38:13 -0500

    Ciclo de vida de componentes: ngOnChanges

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CounterComponent } from './../../../shared/components/counter/counter.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CounterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  duration = signal(1000);
+  message = signal('Hola');
+
+  changeDuration(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber)
+  }
+
+  changeMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value)
+  }
 
 }
