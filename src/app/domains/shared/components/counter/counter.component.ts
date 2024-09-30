a00b7209fddfcf7344955fabb1c424b43f9903fa
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-09-30 17:12:29 -0500

    Evitando memory leaks con ngDestroy

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, Input, signal, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -10,6 +10,8 @@ import { Component, Input, SimpleChange, SimpleChanges } from '@angular/core';
 export class CounterComponent {
   @Input({required:true}) duration = 0;
   @Input({required:true}) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
 
   constructor() {
     // NO ASYNC
@@ -38,6 +40,10 @@ export class CounterComponent {
     console.log('-'.repeat(10));
     console.log('duration =>', this.duration);
     console.log('message =>', this.message);
+    this.counterRef = window.setInterval(() => {
+      console.log('run interval')
+      this.counter.update(statePrev => statePrev + 1);
+    }, 1000)
   }
 
   ngAfterViewInit() {
@@ -50,6 +56,7 @@ export class CounterComponent {
   ngOnDestroy() {
     console.log('ngOnDestroy');
     console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
   }
 
   doSomething() {
