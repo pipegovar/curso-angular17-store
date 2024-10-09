c99effe13cd0ffbfec6f37eb37faded3a09e2212
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-10-09 18:38:31 -0500

    Creando el Header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component';
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { Product } from './../../../shared/models/product.model'
+
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -17,6 +19,27 @@ export class ListComponent {
 
   constructor() {
     const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=11',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=21',
+        creationAt: new Date().toISOString()
+      },
       {
         id: Date.now(),
         title: 'Pro 1',
