c5fdbf528caa6d51f7f838e4339b11a7067aa364
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-11-25 22:33:26 -0500

    Utilizando librerías de JavaScript en Angular

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -2,12 +2,13 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { CommonModule} from '@angular/common';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, ReversePipe],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
