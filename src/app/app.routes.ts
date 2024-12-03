b1edde9522ea7a82f87056c32eec61e423f899d1
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-12-02 23:25:39 -0500

    Vistas anidadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
 
-import {ListComponent} from '@products/pages/list/list.component';
-import {AboutComponent} from '@info/pages/about/about.component';
+import { ListComponent } from '@products/pages/list/list.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
+import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ListComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ListComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+    ]
   },
   {
     path: '**',
