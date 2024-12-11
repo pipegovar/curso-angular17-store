a62147b4b6cdabc758f4ae222f3a192d346c2b17
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-12-11 18:05:28 -0500

    Galería de imagenes

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -14,6 +14,7 @@ export class ProductDetailComponent {
 
   @Input() id?: string;
   product = signal<Product | null>(null);
+  cover = signal("");
   private productService = inject(ProductService);
 
   ngOnInit() {
@@ -22,10 +23,17 @@ export class ProductDetailComponent {
       .subscribe({
         next: (product) => {
           this.product.set(product);
+          if (product.images.length > 0) {
+            this.cover.set(product.images[0])
+          }
         }
       })
     }
 
+  }
+
+  changeCover(newImg: string) {
+    this.cover.set(newImg);
   }
 
 }
--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -1,8 +1,11 @@
-export interface Product{
+import { Category } from "./category.model";
+
+export interface Product {
   id: number;
   title: string;
   description: string;
   price: number;
   images: string[];
   creationAt: string;
+  category: Category;
 }
