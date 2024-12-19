26cad9e1f22812eab13d36ee95aae06686f9e28f
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-12-19 15:57:12 -0500

    Aplicando LazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { ListComponent } from '@products/pages/list/list.component';
-import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 import { LayoutComponent } from '@shared/components/layout/layout.component';
-import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -13,15 +10,15 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        component: ListComponent
+        loadComponent: () => import('@products/pages/list/list.component')
       },
       {
         path: 'about',
-        component: AboutComponent
+        loadComponent: () => import('@info/pages/about/about.component')
       },
       {
         path: 'product/:id',
-        component: ProductDetailComponent
+        loadComponent: () => import('@products/pages/product-detail/product-detail.component')
       },
     ]
   },
